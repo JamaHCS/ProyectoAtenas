de24b0e03664ae1a5f45c445e5c57a450383a018
Jama <jama117@hotmail.com>
2018-06-24 11:19:37 -0500

    Agregamos el tweet/encuesta en el landding. Agregamos boton de scrool up.

--- js/main.js
+++ js/main.js
@@ -168,3 +168,83 @@ $('#btn32').click(function () {
 $('#btn33').click(function () {
     $('#modal33').modal();
 });
+
+
+
+// Bind to scroll
+$(window).scroll(function () {
+
+    //Display or hide scroll to top button 
+    if ($(this).scrollTop() > 100) {
+        $('.scrollup').fadeIn();
+    } else {
+        $('.scrollup').fadeOut();
+    }
+
+    if ($(this).scrollTop() > 130) {
+        $('.navbar').addClass('navbar-fixed-top animated fadeInDown');
+    } else {
+        $('.navbar').removeClass('navbar-fixed-top animated fadeInDown');
+    }
+
+    // Get container scroll position
+    var fromTop = $(this).scrollTop() + topMenuHeight + 10;
+
+    // Get id of current scroll item
+    var cur = scrollItems.map(function () {
+        if ($(this).offset().top < fromTop)
+            return this;
+    });
+
+    // Get the id of the current element
+    cur = cur[cur.length - 1];
+    var id = cur && cur.length ? cur[0].id : "";
+
+    if (lastId !== id) {
+        lastId = id;
+        // Set/remove active class
+        menuItems
+            .parent().removeClass("active")
+            .end().filter("[href=#" + id + "]").parent().addClass("active");
+    }
+});
+
+/*
+Function for scroliing to top
+************************************/
+$('.scrollup').click(function () {
+    $("html, body").animate({
+        scrollTop: 0
+    }, 600);
+    return false;
+});
+
+
+$(window).load(function () {
+    function filterPath(string) {
+        return string.replace(/^\//, '').replace(/(index|default).[a-zA-Z]{3,4}$/, '').replace(/\/$/, '');
+    }
+    $('a[href*=#]').each(function () {
+        if (filterPath(location.pathname) == filterPath(this.pathname) && location.hostname == this.hostname && this.hash.replace(/#/, '')) {
+            var $targetId = $(this.hash),
+                $targetAnchor = $('[name=' + this.hash.slice(1) + ']');
+            var $target = $targetId.length ? $targetId : $targetAnchor.length ? $targetAnchor : false;
+
+            if ($target) {
+
+                $(this).click(function () {
+
+                    //Hack collapse top navigation after clicking
+                    topMenu.parent().attr('style', 'height:0px').removeClass('in'); //Close navigation
+                    $('.navbar .btn-navbar').addClass('collapsed');
+
+                    var targetOffset = $target.offset().top - 63;
+                    $('html, body').animate({
+                        scrollTop: targetOffset
+                    }, 800);
+                    return false;
+                });
+            }
+        }
+    });
+});
